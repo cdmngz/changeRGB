ad030df78486283dd67b7829afd6fd822af98db1
cdmngz <crlsdmngz3@gmail.com>
2020-04-30 12:53:34 +0200

    Cleanest js code

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-document.getElementsByTagName("input")[0].oninput = function() {
-    //Obtiene valor de inputRange No1 y lo inserta en span No1
-    document.getElementsByTagName("span")[0].innerHTML = this.value;
-    colorear();
-}
+const barras = document.getElementsByTagName("input")
 
-document.getElementsByTagName("input")[1].oninput = function() {
-    //Obtiene valor de inputRange No2 y lo inserta en span No2
-    document.getElementsByTagName("span")[1].innerHTML = this.value;
-    colorear();
-}
-
-document.getElementsByTagName("input")[2].oninput = function() {
-    //Obtiene valor de inputRange No3 y lo inserta en span No3
-    document.getElementsByTagName("span")[2].innerHTML = this.value;
-    colorear();
-}
-
-function colorear() {
-    //Obtiene texto de la etiqueta A
-    var textoEnEtiquetaA = document.getElementsByTagName("a")[0].textContent;
-    textoEnEtiquetaA = (textoEnEtiquetaA.replace(/ /g,'')).toLocaleLowerCase();
-    console.log(textoEnEtiquetaA);
-    //Asigna el texto de la etiqueta A en el style del div principal
-    document.getElementById("principal").style.backgroundColor = textoEnEtiquetaA;
+for(const item of barras) {
+    item.oninput = function() {
+        document.getElementsByTagName("span")[this.id].innerText = this.value
+        let textA = document.getElementsByTagName("a")[0].textContent;
+        textA = (textA.replace(/ /g,'')).toLocaleLowerCase();
+        document.getElementById("principal").style.backgroundColor = textA;
+    }
 }
